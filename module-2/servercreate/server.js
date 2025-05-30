cd52ddc66630935d57c567c134aaffa5338d8ca2
Shanto93 <shantoislam7363@gmail.com>
2025-05-30 11:10:05 +0600

    Create server using node.js

---
+++ module-2/servercreate/server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Welcome to server");
+});
+
+server.listen(5000, "127.0.0.1", () => {
+  console.log("Server is running on port 5000");
+});
