0821ffdd3f61de6b59cb6140690649f30b02a75a
Shanto93 <shantoislam7363@gmail.com>
2025-05-30 12:01:32 +0600

    create route for todo and create todo

--- module-2/servercreate/server.js
+++ module-2/servercreate/server.js
@@ -1,7 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Welcome to server");
+  if (req.url === "/todo" && req.method === "GET") {
+    res.end("ALL TODOs");
+  } else if (req.url === "/todo/create-todos" && req.method === "POST") {
+    res.end("TODO created");
+  } else {
+    res.end("Invalid request");
+  }
+});
+
+server.on("error", (error) => {
+  console.error("Server error:", error);
 });
 
 server.listen(5000, "127.0.0.1", () => {
