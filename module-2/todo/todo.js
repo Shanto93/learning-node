408b3a66f67106f650dd3626da79e329543eee21
Shanto93 <shantoislam7363@gmail.com>
2025-05-30 19:03:11 +0600

    Get single TODO using node.js

--- module-2/todo/todo.js
+++ module-2/todo/todo.js
@@ -5,6 +5,8 @@ const filePath = path.join(__dirname, "./db/todo.json");
 const data = fs.readFileSync(filePath, { encoding: "utf8" });
 
 const server = http.createServer((req, res) => {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const pathname = url.pathname;
   // GET all TODO
   if (req.url === "/todo" && req.method === "GET") {
     res.writeHead(200, {
@@ -31,6 +33,24 @@ const server = http.createServer((req, res) => {
       });
       res.end(JSON.stringify(todo, null, 2));
     });
+  }
+  //   GET single TODO
+  else if (req.url.startsWith("/todo") && req.method === "GET") {
+    const title = url.searchParams.get("title");
+    const todos = fs.readFileSync(filePath, { encoding: "utf8" });
+    const parsedTodos = JSON.parse(todos);
+    const todo = parsedTodos.find((todo) => todo.title === title);
+    if (!todo) {
+      res.writeHead(404, {
+        "content-type": "application/json",
+      });
+      return res.end(JSON.stringify({ error: "TODO not found" }));
+    }
+
+    // res.writeHead(200, {
+    //   "content-type": "application/json",
+    // });
+    res.end(JSON.stringify(todo, null, 2));
   } else {
     res.end("Invalid request");
   }
