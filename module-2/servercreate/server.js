a8721483af704ccaf440943987d7fe588a152443
Shanto93 <shantoislam7363@gmail.com>
2025-05-30 12:42:14 +0600

    Change status code and data

--- module-2/servercreate/server.js
+++ module-2/servercreate/server.js
@@ -1,8 +1,21 @@
 const http = require("http");
+const data = [
+  { id: 1, title: "Learn Node.js", completed: false },
+  { id: 2, title: "Build a REST API", completed: false },
+];
 
 const server = http.createServer((req, res) => {
   if (req.url === "/todo" && req.method === "GET") {
-    res.end("ALL TODOs");
+    res.writeHead(201, {
+      "content-type": "application/json",
+      email: "shanto@gmail.com",
+    });
+    res.end(JSON.stringify(data));
+
+    // res.setHeader("Content-Type", "application/json");
+    // res.statusCode = 202;
+    // res.setHeader("email", "default@gmail.com");
+    // res.end(JSON.stringify(data));
   } else if (req.url === "/todo/create-todos" && req.method === "POST") {
     res.end("TODO created");
   } else {
