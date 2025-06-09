c12dabb90166b035f119f2126616990e00f104f6
Shanto93 <shantoislam7363@gmail.com>
2025-06-09 11:25:44 +0600

    Create /todos and /todos/create-todo route and also add a database in folder db and implemented these route

--- module-3/dist/app.js
+++ module-3/dist/app.js
@@ -4,11 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
-const port = 3000;
+const pathname = path_1.default.join(__dirname, "../db/todo.json");
+app.use(express_1.default.json());
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Welcome to Todo");
 });
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.get("/todos", (req, res) => {
+    const data = fs_1.default.readFileSync(pathname, "utf-8");
+    res.json(data);
 });
+app.post("/todos/create-todo", (req, res) => {
+    const { title, body } = req.body;
+    console.log(title, body);
+    res.send("hi");
+});
+exports.default = app;
--- module-3/src/app.ts
+++ module-3/src/app.ts
@@ -1,12 +1,23 @@
 import express, { Application, Request, Response } from "express";
+import fs from "fs";
+import path from "path";
 
 const app: Application = express();
-const port = 3000;
+const pathname = path.join(__dirname, "../db/todo.json");
+
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("Welcome to Todo");
+});
+app.get("/todos", (req: Request, res: Response) => {
+  const data = fs.readFileSync(pathname, "utf-8");
+  res.json(data);
+});
+app.post("/todos/create-todo", (req: Request, res: Response) => {
+  const {title, body} = req.body;
+  console.log(title, body);
+  res.send("hi")
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+export default app;
